e095fba36781ebe747963663fd5aad8b09b92950
Michael Alberter <michael.alberter@outlook.de>
2021-04-25 09:24:26 +0200

    Initial commit

---
+++ worker/index.js
@@ -0,0 +1,27 @@
+const keys = require('./keys');
+
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+
+function fib(index) {
+    if (index == 0) {
+        return 1;
+    }
+    return index * fib(index - 1);
+}
+
+
+sub.on('message', (channel, message) => {
+    console.log(message);
+    redisClient.hset('values', message, fib(parseInt(message)));
+});
+
+sub.subscribe('insert');
