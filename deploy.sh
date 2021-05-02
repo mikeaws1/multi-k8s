ac9f027ba342bb9667a4d8b6580b46f4cc87893c
Michael Alberter <michael.alberter@outlook.de>
2021-05-02 09:20:26 +0200

    Deployment added

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo ${DIGITAL_OCEAN_DROPLET_IP}
+ssh -o StrictHostKeyChecking=no travis@${DIGITAL_OCEAN_DROPLET_IP} << ENDSSH
+  touch droplettest
+  echo "Test sucessful" > droplettest
+  echo $1 >> droplettest
+  echo $1 | docker login --username mikeaws1 --password-stdin
+  echo "Stopping all"
+  docker stop $(docker ps -a -q)
+  echo "Starting up container"
+  docker-compose up --build
+ENDSSH
