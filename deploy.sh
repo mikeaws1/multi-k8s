757fec43b9afb09749a3320dd32a3dbed6d43782
Michael Alberter <michael.alberter@outlook.de>
2021-06-06 10:22:27 +0200

    Added scripts for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
-echo ${DIGITAL_OCEAN_DROPLET_IP}
-ssh -o StrictHostKeyChecking=no travis@${DIGITAL_OCEAN_DROPLET_IP} << ENDSSH
-  touch droplettest
-  echo "Test sucessful" > droplettest
-  echo $1 >> droplettest
-  echo $1 | docker login --username mikeaws1 --password-stdin
-  echo "Stopping all"
-  docker-compose down
-  docker-compose pull
-  echo "Starting up container"
-  docker-compose up --build -d
-ENDSSH
+docker build -t mikeaws1/multi-client:latest -t mikeaws1/multi-client:${SHA} -f ./client/Dockerfile ./client
+docker build -t mikeaws1/multi-server:latest -t mikeaws1/multi-server:${SHA} -f ./server/Dockerfile ./server
+docker build -t mikeaws1/multi-worker:latest -t mikeaws1/multi-worker:${SHA} -f ./worker/Dockerfile ./worker
+
+docker push mikeaws1/multi-client:latest
+docker push mikeaws1/multi-server:latest
+docker push mikeaws1/multi-worker:latest
+
+docker push mikeaws1/multi-client:${SHA}
+docker push mikeaws1/multi-server:${SHA}
+docker push mikeaws1/multi-worker:${SHA}
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=mikeaws1/multi-server:${SHA}
+kubectl set image deployments/client-deployment client=mikeaws1/multi-client:${SHA}
+kubectl set image deployments/worker-deployment worker=mikeaws1/multi-worker:${SHA}
